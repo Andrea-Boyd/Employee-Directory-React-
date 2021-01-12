1b4b9d3c2e1ca9a6a3e78b0091a196b4a1029525
Andrea Boyd <andreanboyd@yahoo.com>
2021-01-11 22:08:46 -0600

    sorting

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import Api from '../../utils/Api'
 
 class Info extends React.Component {
-    state ={
+    state = {
         users: [],
         filter: [],
-        sorting: [],
+        sorted: false,
         search: ""
     }
 
-    componentDidMount () {
+    componentDidMount() {
         this.loadUsers();
     }
 
@@ -17,70 +17,75 @@ class Info extends React.Component {
         Api.getUsers()
             .then(res => this.setState({ users: res.data.results }))
             .then(res => this.setState({ filter: this.state.users }))
-            .then(res => this.setState({ sorting: this.state.users }))
+            .then(res => this.setState({ sorted: false }))
     }
-    
+
     sortTable = (event) => {
-        // event.preventDefault();
-        console.log(this.state.sorting)
-        let sortArr = this.state.sorting;
+        console.log(this.state.sorted)
+        let sortArr = this.state.users;
+        console.log(sortArr)
+        if (this.state.sorted === false) {
+            this.setState({ sorted: true })
+            sortArr.sort(function (a, b) {
+                let nameA = a.name.first.toUpperCase();
+                let nameB = b.name.first.toUpperCase();
 
-        sortArr.sort(function(a, b)  {
-            let nameA = a.name.first.toUpperCase();
-            let nameB = b.name.first.toUpperCase();
+                if (nameA > nameB) {
+                    return 1;
+                }
+                if (nameA < nameB) {
+                    return -1;
+                }
 
-            if(nameA > nameB) {
-                return 1;
-            }
-            if(nameA < nameB) {
-                return -1;
-            }
+                return 0;
+            })
+        } else {
+            this.setState({ sorted: false })
+        }
 
-            return 0;
-        })
-        this.setState({filter: sortArr})
+        this.setState({ filter: sortArr })
     }
 
     handleInputChange = (event) => {
-        const value = event.target.value;
+        const value = event.target.value.toLowerCase();
         // console.log(value);
-        const filtered = this.state.users.filter(query => query.name.first.includes(value) || query.name.last.includes(value) || query.phone.includes(value));
+        const filtered = this.state.users.filter(query => query.name.first.toLowerCase().includes(value) || query.name.last.toLowerCase().includes(value) || query.phone.includes(value));
         this.setState({ filter: filtered })
     }
 
-    render(){
+    render() {
         return (
             <>
-        <nav className="navbar navbar-dark bg-dark justify-content-center">
-            <form className="form-inline">
-                <input className="form-control mr-sm-2" type="search" onChange={this.handleInputChange} placeholder="Enter a name"/>
-            </form>
-        </nav>
-        <table class="table table-striped table-dark">
-            <thead>
-                <tr>
-                    <th scope="col">Photo</th>
-                    <th  onClick={this.sortTable} scope="col">Name</th>
-                    <th scope="col">Phone</th>
-                    <th scope="col">Email</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    this.state.filter.map(element => (
-                    <tr>
-                        <td scope="row"><img src={element.picture.thumbnail} alt="1"/></td>
-                        <td>{element.name.first} {element.name.last}</td>
-                        <td>{element.phone}</td>
-                        <td>{element.email}</td>
-                    </tr>
-                ))
-                    }
-            </tbody>
-        </table>
-    </>
-    )
-}
+                <nav className="navbar navbar-dark bg-dark justify-content-center">
+                    <form className="form-inline">
+                        <input className="form-control mr-sm-2" type="search" onChange={this.handleInputChange} placeholder="Enter a name" />
+                    </form>
+                </nav>
+                <table class="table table-striped table-dark">
+                    <thead>
+                        <tr>
+                            <th scope="col">Photo</th>
+                            <th onClick={this.sortTable} scope="col">Name</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.filter.map(element => (
+                                <tr>
+                                    <td scope="row"><img src={element.picture.thumbnail} alt="1" /></td>
+                                    <td>{element.name.first} {element.name.last}</td>
+                                    <td>{element.phone}</td>
+                                    <td>{element.email}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </>
+        )
+    }
 }
 
 export default Info; 
